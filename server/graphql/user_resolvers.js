4b95ed5f041c2419ba9de468cdc976ccda667ac3
Devon Harris <dbharris2@gmail.com>
2018-02-26 20:58:05 -0800

    [step-up] Use remote mongodb

--- server/graphql/user_resolvers.js
+++ server/graphql/user_resolvers.js
@@ -1,8 +1,8 @@
 import {ObjectId} from 'mongodb'
 
 const userQueryResolvers = {
-  user: async (root, {_id}, context) => {
-    return await context.db.collection('users').findOne(ObjectId(_id));
+  user: async (root, {id}, context) => {
+    return await context.db.collection('users').findOne(ObjectId(id));
   },
   users: async (root, args, context) => {
     return await context.db.collection('users').find({}).toArray();
--- server/index.js
+++ server/index.js
@@ -9,14 +9,12 @@ const schema = require('./graphql/schema').schema
 
 const app = express();
 
-const MONGO_URL = 'mongodb://localhost:27017/fitbit';
-
 app.set('port', process.env.PORT || 8080);
 app.use(cors());
 
 export const start = async () => {
-  const db = await MongoClient.connect(MONGO_URL, (err, client) => {
-    const db = client.db('fitbit');
+  const db = await MongoClient.connect(process.env.MONGODB_URI, (err, client) => {
+    const db = client.db('stepup');
     app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
     app.use('/', bodyParser.json(), graphqlExpress({
       schema,
