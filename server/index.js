3b41dd26ef57b71cbb64998e1c2c4f4cdba3b2b6
Devon Harris <dbharris2@gmail.com>
2018-02-27 20:34:01 -0800

    [step-up] Add Fitbit authentication and callback endpoints

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
+import FitbitApiClient from 'fitbit-node';
 import {MongoClient} from 'mongodb';
 
 const graphqlExpress = require('graphql-server-express').graphqlExpress
@@ -12,14 +13,40 @@ const app = express();
 app.set('port', process.env.PORT || 8080);
 app.use(cors());
 
+app.get('/authenticate', async (req, res) => {
+  const client = new FitbitApiClient({
+  	clientId: process.env.APP_ID,
+  	clientSecret: process.env.APP_SECRET,
+  	apiVersion: '1.2',
+  });
+  const authorizeUrl = client.getAuthorizeUrl(
+    'activity heartrate location profile settings sleep social',
+    process.env.FITBIT_AUTHORIZATION_CALLBACK_URL,
+  );
+  res.redirect(authorizeUrl);
+});
+
+app.get('/fitbit-callback', async (req, res) => {
+  const client = new FitbitApiClient({
+    clientId: process.env.APP_ID,
+  	clientSecret: process.env.APP_SECRET,
+  	apiVersion: '1.2',
+  });
+  const accessToken = await client.getAccessToken(
+    req.query.code,
+    process.env.FITBIT_AUTHORIZATION_CALLBACK_URL,
+  );
+  const profile = await client.get('/profile.json', accessToken.access_token);
+  res.send(profile[0]);
+});
+
 export const start = async () => {
-  const db = await MongoClient.connect(process.env.MONGODB_URI, (err, client) => {
-    const db = client.db('stepup');
+  await MongoClient.connect(process.env.MONGODB_URI, (err, client) => {
     app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
     app.use('/', bodyParser.json(), graphqlExpress({
       schema,
       context: {
-        db: db,
+        db: client.db('stepup'),
       }
     }));
     app.listen(app.get('port'), async () => {
