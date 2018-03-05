69f2a63f50bc66b7055c871a881426dcb74d0e70
Devon Harris <dbharris2@gmail.com>
2018-03-04 20:55:31 -0800

    [step-up] Serve html

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
 import {MongoClient} from 'mongodb';
+import path from 'path';
 
 const graphqlExpress = require('graphql-server-express').graphqlExpress
 const graphiqlExpress = require('graphql-server-express').graphiqlExpress
@@ -15,9 +16,13 @@ const app = express();
 app.set('port', process.env.PORT || 8080);
 app.use(cors());
 
-console.log('auth url: ' + process.env.FITBIT_AUTHORIZATION_CALLBACK_URL);
-
 const stepup_service = new StepUpService();
+
+app.use(express.static(path.resolve(__dirname, '..', 'ui', 'build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'ui', 'build', 'index.html'));
+});
 
 app.get('/authenticate', async (req, res) => {
   console.log("Authenticate");
@@ -64,7 +69,7 @@ export const start = async () => {
       }
     }));
     app.listen(app.get('port'), async () => {
-      console.log('Server started: http://localhost:' + app.get('port') + '/graphiql');
+      console.log('Server started: http://localhost:' + app.get('port'));
     });
   });
 };
