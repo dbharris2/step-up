ab944fd594eedea79f988ca5ab1613883c833034
Caroline Modic <CAROLINE.MODIC@CLEVER.COM>
2018-03-06 20:28:00 -0800

    animate progress bars

--- ui/src/ProgressBar.js
+++ ui/src/ProgressBar.js
@@ -1,20 +1,49 @@
 import React, { Component } from 'react';
 import './ProgressBar.css';
+import * as ReactDOM from "react-dom";
+
+import * as anime from "animejs";
 
 class ProgressBar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentProgressValue: 0
+        };
+    }
+
+    componentDidMount() {
+        this.initAnimation();
+    }
+
+    initAnimation() {
+        let percent = (this.props.steps / this.props.tiers[this.props.tiers.length - 1]) * 100;
+        anime({
+            targets: this.markerRef,
+            width: `${percent}%`,
+            duration: 550,
+            delay: 750,
+            easing: 'linear'
+        });
+    }
+
+    handleMarkerRef(ref) {
+        this.markerRef = ReactDOM.findDOMNode(ref);
+    };
+
     render() {
         let beatTier = 0
-        let finalTier = 0
         let i = 0
         for (let tier of this.props.tiers) {
             i += 1
-            finalTier = tier
             if (this.props.steps >= tier) {
                 beatTier = i
             }
         }
-        let percent = (this.props.steps / finalTier) * 100;
-        let style = {"width": `${percent}%`};
+        //let style = {"width": `${percent}%`};
+        let style = {"width": `0%`}
+
         switch (beatTier) {
             case 0:
                 style["backgroundColor"] = "#EB3B49";
@@ -25,7 +54,7 @@ class ProgressBar extends Component {
             case 2:
                 style["backgroundColor"] = "#01D59A";
                 break;
-            case 3:
+            default:
                 style["backgroundColor"] = "#E838BF";
                 break;
         }
@@ -33,7 +62,7 @@ class ProgressBar extends Component {
             <div className="ProgressBar-Wrapper">
                 {this.props.steps} steps
                 <div className="ProgressBar-Container">
-                    <div className="ProgressBar-FilledIn" style={style}> </div>
+                    <div className="ProgressBar-FilledIn" style={style} ref={(ref) => this.handleMarkerRef(ref)}> </div>
                 </div>
             </div>
         );
